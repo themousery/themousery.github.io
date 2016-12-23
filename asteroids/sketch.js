a9d78f3bd1b931f6fcee6e83ade782da642e4816
themousery <mouserygames@gmail.com>
2016-12-23 15:57:31 -0800

    fixed collions in asteroids

--- asteroids/sketch.js
+++ asteroids/sketch.js
@@ -103,23 +103,6 @@ function draw() {
     if (asteroids[i].y >= H) {
       asteroids.splice(i, 1);
     }
-    
-    if (collideRectRect(player.x, player.y, 44, 70, asteroids[i].x, asteroids[i].y, 46, 38)) {
-      player.hp -= 1;
-      hit.play();
-      asteroids.splice(i, 1);
-      hud.bleed = true;
-      break;
-    }
-    for (var j = 0; j < lasers.length; j++) {
-      if (collideRectRect(asteroids[i].x, asteroids[i].y, 46, 38, lasers[j].x, lasers[j].y, 3, 10)) {
-        score += 1;
-        explosion.play();
-        asteroids.splice(i, 1);
-        lasers.splice(j, 1);
-        break;
-      }
-    }
   }
   
   for (i = 0; i < powerups.length; i++) {
@@ -128,13 +111,6 @@ function draw() {
     
     if (powerups[i].y >= H) {
       powerups.splice(i, 1);
-    }
-    
-    if (collideRectRect(player.x, player.y, 44, 70, powerups[i].x, powerups[i].y, 16, 15)) {
-      powerups.splice(i, 1);
-      player.hp += 1;
-      powerup.play();
-      break;
     }
   }
   
@@ -145,14 +121,6 @@ function draw() {
     if (alasers[i].y > H) {
       alasers.splice(i, 1);
     }
-    
-    if (collideRectRect(player.x, player.y, 44, 70, alasers[i].x, alasers[i].y, 3, 10)) {
-      player.hp -= 1;
-      hit.play();
-      alasers.splice(i, 1);
-      hud.bleed = true;
-      break;
-    }
   }
   
   for (i = 0; i < aliens.length; i++) {
@@ -162,15 +130,6 @@ function draw() {
     if (aliens[i].hp <= 0) {
       aliens.splice(i, 1);
       score += 10;
-    }
-    
-    for (j = 0; j < lasers.length; j++) {
-      if (collideRectRect(aliens[i].x, aliens[i].y, 92, 48, lasers[j].x, lasers[j].y, 3, 10)) {
-        aliens[i].hp -= 1;
-        explosion.play();
-        lasers.splice(j, 1);
-        break;
-      }
     }
   }
   
