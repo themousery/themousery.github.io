f94453fe0f7a1df375cc5c87a45a20d590421c7d
themousery <mouserygames@gmail.com>
2016-12-30 00:20:43 -0800

    just testing something out

--- asteroids/alien.js
+++ asteroids/alien.js
@@ -1,28 +1,22 @@
 function Alien() {
   this.img = ufoimg;
   this.speed = int(random(5,7));
-  temp = [-182, W+90];
-  this.x = temp[Math.floor(Math.random() * 2)];
-  this.y = int(random(50, 200));
+  this.x = choice([-182, W+90]);
+  this.y = randint(50, 200);
   this.laserfire = counter/60;
   this.hp = 3;
-  
+
   this.update = function() {
     this.x += this.speed;
-    
+
     if (this.x >= W-92) {
       this.speed = -abs(this.speed);
     }
     else if (this.x <= 0) {
       this.speed = abs(this.speed);
     }
-    
-    if (int(counter/60) !== this.laserfire) {
-      this.laserfire = counter/60;
-      alasers.push(new AlienLaser(this.x,this.y));
-    }
   }
-  
+
   this.show = function() {
     image(this.img, this.x, this.y);
   }
