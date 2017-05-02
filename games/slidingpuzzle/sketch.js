21129dd212f494cf371079ece5d44d47a9683edd
themousery <mouserygames@gmail.com>
2017-05-01 20:20:41 -0700

    fixed sliding puzzle

--- games/slidingpuzzle/sketch.js
+++ games/slidingpuzzle/sketch.js
@@ -70,3 +70,13 @@ function draw() {
     winScreen();
   }
 }
+
+//prevent scrolling on mobile
+document.addEventListener('touchstart', this.touchstart);
+document.addEventListener('touchmove', this.touchmove);
+function touchstart(e) {
+    e.preventDefault()
+}
+function touchmove(e) {
+    e.preventDefault()
+}
