884e2f6d3180917b6f597f2ebea71812ddbdae3e
themousery <mouserygames@gmail.com>
2017-05-02 20:07:47 -0700

    updated living art a lot

--- games/livingart/sketch.js
+++ games/livingart/sketch.js
@@ -1,32 +1,66 @@
+function preload() {
+  font = loadFont("/index_files/Stellar.otf");
+}
+
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
+  off = createGraphics(displayWidth, displayHeight);
   fireflies = [];
   background(0);
   pause = false;
   sel = createSelect();
-  sel.option('free-draw');
-  sel.option('colonies');
+  sel.option('random');
+  sel.option('left');
+  sel.option('right');
+  sel.option('up');
+  sel.option('down');
+  sel.option('follower');
   sel.position(width-90, 5);
+  textFont(font);
+  textSize(25);
+}
+
+function showPause() {
+  noStroke();
+  if (mouseX < 75 && mouseY < 75) {
+    fill(0,0,255);
+  }
+  else {
+    fill(255);
+  }
+  if (!pause) {
+    text("pause", 20,40);
+  }
+  else {
+    text("play", 20,40);
+  }
+}
+
+function showClear() {
+  if (mouseX > 75 && mouseX < 175 && mouseY < 75) {
+    fill(0,0,255);
+  }
+  else {
+    fill(255);
+  }
+  text("clear",100,40);
 }
 
 function draw() {
+  background(0)
+  image(off,0,0)
   for (i = 0; i < fireflies.length; i++) {
     if (!pause) {
       fireflies[i].update();
     }
     fireflies[i].show();
-    // if (fireflies[i].x < 0 || fireflies[i].y < 0 || fireflies[i].x > width || fireflies[i].y > height) {
-    //   fireflies.splice(i, 1);
-    // }
   }
+  showPause();
+  showClear();
 }
 
 function windowResized() {
-  temp = createGraphics(width, height);
-  temp.image(cnv);
-  resizeCanvas(windowWidth, windowHeight);
-  background(0);
-  image(temp);
+  resizeCanvas(windowWidth,windowHeight)
   sel.position(width-90, 5);
 }
 
@@ -35,10 +69,14 @@ function diff(v1, v2) {
 }
 
 function mousePressed() {
-  if (sel.value() === ('colonies') && !(mouseX>width-90 && mouseY<20)) {
-    for (i = 0; i < 100; i++) {
-      fireflies.push(new Firefly(mouseX, mouseY));
+  if (mouseX < 75 && mouseY < 75) {
+    pause = !pause;
+  }
+  else if (mouseX > 75 && mouseX < 175 && mouseY < 75) {
+    if (confirm("Are you sure you want to clear the screen?")) {
+      off.background(0);
     }
+    fireflies = [];
   }
 }
 
@@ -49,19 +87,8 @@ function keyPressed() {
 }
 
 function mouseDragged() {
-  if (sel.value() === 'free-draw' && !(mouseX>width-90 && mouseY<20)) {
-    fireflies.push(new Firefly(mouseX, mouseY));
+  if (!(mouseX>width-90 && mouseY<20) && !(mouseX < 175 && mouseY < 75)) {
+    fireflies.push(new Firefly(mouseX, mouseY, sel.value()));
   }
   return false
 }
-
-
-//prevent scrolling on mobile
-// document.addEventListener('touchstart', this.touchstart);
-// document.addEventListener('touchmove', this.touchmove);
-// function touchstart(e) {
-//     e.preventDefault()
-// }
-// function touchmove(e) {
-//     e.preventDefault()
-// }
